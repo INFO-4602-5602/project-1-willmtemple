3d6ff05f51c419cd253be5ca9016a1808a3b34fc
William Temple <William.Temple@colorado.edu>
2018-02-14 20:30:27 -0700

    begin adaptation
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- anscombe.js
+++ anscombe.js
@@ -3,73 +3,6 @@
 
 // Assumes that jQuery has been imported as `$` in this scope
 
-// I'm using a javascript-ified version of the data rather than d3.csv to load it.
-const AQ = [
-    // I
-    [
-        {x: 10.0, y: 8.04},
-        {x: 8.0, y: 6.95},
-        {x: 13.0, y: 7.58},
-        {x: 9.0, y: 8.81},
-        {x: 11.0, y: 8.33},
-        {x: 14.0, y: 9.96},
-        {x: 6.0, y: 7.24},
-        {x: 4.0, y: 4.26},
-        {x: 12.0, y: 10.84},
-        {x: 7.0, y: 4.82},
-        {x: 5.0, y: 5.68}
-    ],
-    // II
-    [
-        {x: 10.0, y: 9.14},
-        {x: 8.0, y: 8.14},
-        {x: 13.0, y: 8.74},
-        {x: 9.0, y: 8.77},
-        {x: 11.0, y: 9.26},
-        {x: 14.0, y: 8.10},
-        {x: 6.0, y: 6.13},
-        {x: 4.0, y: 3.10},
-        {x: 12.0, y: 9.13},
-        {x: 7.0, y: 7.26},
-        {x: 5.0, y: 4.74}
-    ],
-    // III
-    [
-        {x: 10.0, y: 7.46},
-        {x: 8.0, y: 6.77},
-        {x: 13.0, y: 12.74},
-        {x: 9.0, y: 7.11},
-        {x: 11.0, y: 7.81},
-        {x: 14.0, y: 8.84},
-        {x: 6.0, y: 6.08},
-        {x: 4.0, y: 5.39},
-        {x: 12.0, y: 8.15},
-        {x: 7.0, y: 6.42},
-        {x: 5.0, y: 5.73}
-    ],
-    // IV
-    [
-        {x: 8.0, y: 6.58},
-        {x: 8.0, y: 5.76},
-        {x: 8.0, y: 7.71},
-        {x: 8.0, y: 8.84},
-        {x: 8.0, y: 8.47},
-        {x: 8.0, y: 7.04},
-        {x: 8.0, y: 5.25},
-        {x: 19.0, y: 12.50},
-        {x: 8.0, y: 5.56},
-        {x: 8.0, y: 7.91},
-        {x: 8.0, y: 6.89}
-    ]
-];
-
-const Y_MAX = Math.max(
-    d3.max(AQ[0], function(d) { return d.y }),
-    d3.max(AQ[1], function(d) { return d.y }),
-    d3.max(AQ[2], function(d) { return d.y }),
-    d3.max(AQ[3], function(d) { return d.y })
-);
-
 // If you change these, also change padding for qcon in anscombe.css
 const margin = { top: 20, left: 20, bottom: 20, right: 20 };
 
@@ -78,6 +11,18 @@ function getDims(div) {
         height: div.height(),
         width: div.width()
     }
+}
+
+function toObjArray(csvData) {
+    var data = [];
+    for (i = 0; i < csvData.length; i++)
+    {
+        var newO = {};
+        newO.x = + csvData[i]["x"];
+        newO.y = + csvData[i]["y"];
+        data[i] = newO;
+    }
+    return data;
 }
 
 // http://www.statisticshowto.com/probability-and-statistics/regression-analysis/find-a-linear-regression-equation/
@@ -116,7 +61,7 @@ function plot(title, divId, data) {
     };
 
     var xscale = d3.scaleLinear().range([0, svgDims.width]);
-    var yscale = d3.scaleLinear().range([0, svgDims.height]);
+    var yscale = d3.scaleLinear().range([svgDims.height, 0]);
 
     // Create the svg element
     var svg = d3.select("div #" + divId)
@@ -129,7 +74,7 @@ function plot(title, divId, data) {
     var xExtent = d3.extent(data, function(d) { return d.x; });
     var extendedxExtent = [xExtent[0]-1, xExtent[1]+1];
     xscale.domain(extendedxExtent);
-    yscale.domain([Y_MAX + 1, 0]);
+    yscale.domain([0, d3.max(data, function(d) { return d.y; }) + 1]);
 
     svg.selectAll("circle")
         .data(data)
@@ -166,13 +111,28 @@ function plot(title, divId, data) {
     // title
     svg.append("text")
         .attr("y", margin.top / 2 )
-        .attr("x", svgDims.width / 2)
-        .style("text-achnor", "middle")
+        .attr("x", (svgDims.width - margin.left - margin.right) / 2)
+        .attr("text-achnor", "middle")
         .style("font-size", "16pt")
-        .text("Set " + title);
+        .text(title);
 }
 
-plot("Anscombe I", "aq1", AQ[0]);
-plot("Anscombe II", "aq2", AQ[1]);
-plot("Anscombe III", "aq3", AQ[2]);
-plot("Anscombe IV", "aq4", AQ[3]);
+d3.csv("data/anscombe_I.csv", function(error, data) {
+    if (error) throw error;
+    plot("Anscombe I", "aq1", toObjArray(data));
+});
+
+d3.csv("data/anscombe_II.csv", function(error, data) {
+    if (error) throw error;
+    plot("Anscombe II", "aq1", toObjArray(data));
+});
+
+d3.csv("data/anscombe_III.csv", function(error, data) {
+    if (error) throw error;
+    plot("Anscombe III", "aq1", toObjArray(data));
+});
+
+d3.csv("data/anscombe_IV.csv", function(error, data) {
+    if (error) throw error;
+    plot("Anscombe IV", "aq1", toObjArray(data));
+});
